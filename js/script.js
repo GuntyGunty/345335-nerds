15763568ee4643e2eed22675d7674390a4a3210b
GuntyGunty <Gunter0705@yandex.ru>
2017-02-17 18:06:50 +0300

    Внесла правки

--- js/script.js
+++ js/script.js
@@ -1,32 +1,16 @@
 
-var link = document.querySelector(".contact-link");
+var link = document.querySelector(".contact-button");
 var popup = document.querySelector(".modal-content");
 var close = popup.querySelector(".modal-content-close");
 var form = popup.querySelector("form");
-var login = popup.querySelector("[name=name]");
-var email = popup.querySelector("[name=email]");
-var storage = localStorage.getItem("login");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("modal-content-show");
-  if (storage) {
-         login.value = storage;
-         password.focus();
-       } else {
-         login.focus();
-       }
+
 });
   close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("modal-content-show");
 
-});
-form.addEventListener("submit", function(event) {
-  if (!login.value || !email.value) {
-  event.preventDefault();
-  console.log("лалала");
-} else {
-  localStorage.setItem("login", login.value);
-}
 });
